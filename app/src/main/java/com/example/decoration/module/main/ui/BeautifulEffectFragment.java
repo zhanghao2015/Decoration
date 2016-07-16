66ca6c78baab550cce165de423c00b10d8372c00
williamliu <williamliuok@sina.cn>
2016-07-16 23:38:39 +0800

    主界面基础布局框架完成

---
+++ app/src/main/java/com/example/decoration/module/main/ui/BeautifulEffectFragment.java
@@ -0,0 +1,37 @@
+package com.example.decoration.module.main.ui;
+
+import android.view.View;
+
+import com.example.decoration.R;
+import com.example.decoration.base.BaseFragment;
+
+/**
+ * Created by Administrator on 2016/7/16.
+ */
+public class BeautifulEffectFragment extends BaseFragment{
+    @Override
+    protected int setViewID() {
+        return R.layout.layout_beautifuleffect_fragment;
+//
+    }
+
+    @Override
+    protected void findView(View view) {
+
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    protected void initEvent() {
+
+    }
+
+    @Override
+    protected void loadDate() {
+
+    }
+}
---
+++ app/src/main/java/com/example/decoration/module/main/ui/HomeFragment.java
@@ -0,0 +1,36 @@
+package com.example.decoration.module.main.ui;
+
+import android.view.View;
+
+import com.example.decoration.R;
+import com.example.decoration.base.BaseFragment;
+
+/**
+ * Created by Administrator on 2016/7/16.
+ */
+public class HomeFragment extends BaseFragment{
+    @Override
+    protected int setViewID() {
+        return R.layout.layout_home_fragment;
+    }
+
+    @Override
+    protected void findView(View view) {
+
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    protected void initEvent() {
+
+    }
+
+    @Override
+    protected void loadDate() {
+
+    }
+}
