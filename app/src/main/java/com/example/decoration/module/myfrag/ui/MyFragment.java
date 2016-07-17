fd14213b6316dc615fa405055b9b81aed7d086af
zhanghao2015 <841459485@qq.com>
2016-07-17 22:06:45 +0800

    我的Fragment 写好了一级界面 列出了是否登录的逻辑判断

--- app/src/main/java/com/example/decoration/module/myfrag/ui/MyFragment.java
+++ app/src/main/java/com/example/decoration/module/myfrag/ui/MyFragment.java
@@ -2,33 +2,79 @@ package com.example.decoration.module.myfrag.ui;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
-import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.example.decoration.MyApplication;
 import com.example.decoration.R;
 import com.example.decoration.base.BaseFragment;
 import com.example.decoration.modules.main.ui.MainActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * Created by Administrator on 2016/7/16.
  */
-public class MyFragment extends BaseFragment{
+public class MyFragment extends BaseFragment  {
     ImageView myself_translate_iv1;
     ImageView myself_translate_iv2;
     ImageView myself_translate_iv3;
     Button loginbtn;
     Activity activity;
-    private boolean isAlive=true;
+    @BindView(R.id.user_circular)
+    ImageView userCircular;
+    @BindView(R.id.makeCold)
+    Button makeCold;
+    @BindView(R.id.mymoneypack)
+    Button mymoneypack;
+    @BindView(R.id.dingdan)
+    ImageView dingdan;
+    @BindView(R.id.riji)
+    ImageView riji;
+    @BindView(R.id.myrizi)
+    RelativeLayout myrizi;
+    @BindView(R.id.talk)
+    ImageView talk;
+    @BindView(R.id.mytalk)
+    RelativeLayout mytalk;
+    @BindView(R.id.love)
+    ImageView love;
+    @BindView(R.id.myshoucang)
+    RelativeLayout myshoucang;
+    @BindView(R.id.advance)
+    ImageView advance;
+    @BindView(R.id.myadvance)
+    RelativeLayout myadvance;
+    @BindView(R.id.tousu)
+    ImageView tousu;
+    @BindView(R.id.mytousu)
+    RelativeLayout mytousu;
+    @BindView(R.id.contact)
+    ImageView contact;
+    @BindView(R.id.mykefu)
+    RelativeLayout mykefu;
+    @BindView(R.id.setting)
+    ImageView setting;
+    @BindView(R.id.mysetting)
+    RelativeLayout mysetting;
+    @BindView(R.id.conversionIv)
+    ImageView conversionIv;
+    private boolean isAlive = true;
     private TranslateAnimation animLoading;
     private TranslateAnimation animHiddening;
+
 
     @Override
     protected int setViewID() {
@@ -39,20 +85,21 @@ public class MyFragment extends BaseFragment{
 
     @Override
     protected void findView(View view) {
-        myself_translate_iv1= (ImageView) view.findViewById(R.id.myself_translate_iv1);
-        myself_translate_iv2= (ImageView) view.findViewById(R.id.myself_translate_iv2);
-        myself_translate_iv3= (ImageView) view.findViewById(R.id.myself_translate_iv3);
-        loginbtn= (Button) view.findViewById(R.id.login_btn);
+        myself_translate_iv1 = (ImageView) view.findViewById(R.id.myself_translate_iv1);
+        myself_translate_iv2 = (ImageView) view.findViewById(R.id.myself_translate_iv2);
+        myself_translate_iv3 = (ImageView) view.findViewById(R.id.myself_translate_iv3);
+        loginbtn = (Button) view.findViewById(R.id.login_btn);
     }
 
     @Override
     protected void init() {
-        activity=MainActivity.mainActivity;
+        activity = MainActivity.mainActivity;
         initAnimation();
 
     }
+
     /**
-     *一张动画循环左右平移
+     * 一张动画循环左右平移
      */
     @Override
     protected void initEvent() {
@@ -61,29 +108,32 @@ public class MyFragment extends BaseFragment{
 //        startLoadingAnim(myself_translate_iv3);
 
 
-        new Thread(new Runnable(){
+        new Thread(new Runnable() {
             int what = 1;
+
             @Override
             public void run() {
-                while(isAlive){
+                while (isAlive) {
                     try {
                         Thread.sleep(4000);
                         Message msg = mHandler.obtainMessage(what);
                         mHandler.sendMessage(msg);
-                        what ++;
+                        what++;
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
-            }}).start();
+            }
+        }).start();
 
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(activity,LoginActivity.class);
+                Intent intent = new Intent(activity, LoginActivity.class);
                 startActivity(intent);
             }
         });
+
     }
 
     @Override
@@ -94,43 +144,45 @@ public class MyFragment extends BaseFragment{
     /**
      * 图片无限循环左右平移
      */
-    protected  void initAnimation(){
-       WindowManager windowManager= (WindowManager) MyApplication.context.getSystemService(MyApplication.context.WINDOW_SERVICE);
+    protected void initAnimation() {
+        WindowManager windowManager = (WindowManager) MyApplication.context.getSystemService(MyApplication.context.WINDOW_SERVICE);
         int screenWidth = windowManager.getDefaultDisplay().getWidth();
-        Log.d("QF","屏幕宽度="+screenWidth);
+        Log.d("QF", "屏幕宽度=" + screenWidth);
         //从屏幕右边 2s内移动到最左边
-        animLoading = new TranslateAnimation(screenWidth,0,0,0);
+        animLoading = new TranslateAnimation(screenWidth, 0, 0, 0);
         animLoading.setDuration(5000);
         //从屏幕原点 往左边走
-        animHiddening = new TranslateAnimation(0,-screenWidth,0,0);
+        animHiddening = new TranslateAnimation(0, -screenWidth, 0, 0);
         animHiddening.setDuration(5000);
 
     }
-    private void startLoadingAnim(ImageView imageView){
+
+    private void startLoadingAnim(ImageView imageView) {
         imageView.startAnimation(animLoading);
         imageView.setVisibility(View.VISIBLE);
     }
 
-    private void startHiddeningAnim(ImageView imageView){
+    private void startHiddeningAnim(ImageView imageView) {
         imageView.startAnimation(animHiddening);
         imageView.setVisibility(View.GONE);
     }
-    Handler mHandler = new Handler(){
+
+    Handler mHandler = new Handler() {
         @Override
-        public void handleMessage(Message msg){
-            int flag = msg.what%4;
-            Log.d("QF","flag=="+flag);
-           // Log.i("QF", "handleMessage(Message msg) -- " +"被执行...");
-            if(flag == 0){
+        public void handleMessage(Message msg) {
+            int flag = msg.what % 4;
+            Log.d("QF", "flag==" + flag);
+            // Log.i("QF", "handleMessage(Message msg) -- " +"被执行...");
+            if (flag == 0) {
                 startLoadingAnim(myself_translate_iv1);
                 startHiddeningAnim(myself_translate_iv2);
-            }else if(flag == 1){
+            } else if (flag == 1) {
                 startLoadingAnim(myself_translate_iv2);
                 startHiddeningAnim(myself_translate_iv1);
-            }else if(flag == 2){
+            } else if (flag == 2) {
                 startLoadingAnim(myself_translate_iv1);
                 startHiddeningAnim(myself_translate_iv2);
-            }else if(flag == 3){
+            } else if (flag == 3) {
                 startLoadingAnim(myself_translate_iv2);
                 startHiddeningAnim(myself_translate_iv1);
             }
@@ -139,7 +191,31 @@ public class MyFragment extends BaseFragment{
 
     @Override
     public void onDestroy() {
-        isAlive=false;
+        isAlive = false;
         super.onDestroy();
     }
+
+
+    /**
+     * 我的界面 没有登录的情况
+     * 1.信息图片 点击登录 我的钱包 我的日记 我的话题 我的收藏  我的投诉 都是跳转到登录界面
+     * 2.赚曲装修基金 跳到 获取专属优惠码的界面
+     * 3.我的订单 先是跳到我的订单界面 在跳到登录界面
+     * 4.意见反馈 跳到反馈界面需要手动填写自己号码
+     *
+     * 我的界面 登录的情况
+     * 1.信息图片跳到消息界面
+     * 2.圆形头像 跳到个人资料界面
+     * 3.点击登录按钮 变成 请设置昵称
+     * 4.赚曲装修基金 跳到 已经生存优惠码的界面
+     * 5.我的订单跳到预约装修界面
+     * 6.我的日记 跳到我的日子界面
+     * 7.我的话题跳到 发表帖子界面
+     * 8.我的收藏 跳到收藏界面
+     * 9.意见反馈跳到自动获取手机号码 反馈界面
+     * 10.我的投诉 跳到 投诉界面
+     *
+     * 联系客服和设置项与登录逻辑无关
+     */
+
 }
